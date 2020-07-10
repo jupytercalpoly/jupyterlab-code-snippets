026d00bab40a13bf6922e186ee39ca9a74e251dc
jahn <aju960219@gmail.com>
2020-07-10 15:38:53 -0700

    Create search/filter bar

---
+++ src/SearchBar.tsx
@@ -0,0 +1,35 @@
+import { InputGroup } from '@jupyterlab/ui-components';
+import React from 'react';
+
+interface ISearchProp {
+    onFilter: (filterValue: string) => void;
+}
+
+export class SearchBar extends React.Component<ISearchProp> {
+    state = {
+        value: ""
+    }
+    
+    updateValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ value: event.target.value }, this.filterSnippets);
+    };
+
+    filterSnippets = () => {
+        this.props.onFilter(this.state.value);
+    };
+
+    render() {
+        return (
+            <div className="jp-codesnippet-searchbar">
+                <InputGroup 
+                    className="jp-codesnippet-searchwrapper"
+                    type="text"
+                    placeholder="Type Here..."
+                    onChange={this.updateValue}
+                    rightIcon="search"
+                    value={this.state.value}
+                />
+            </div>
+        );
+    }
+}
