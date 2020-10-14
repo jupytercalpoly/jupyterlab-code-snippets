e50ea06f3e54f6430abbefb0cf4520585d51afa6
Kiran <kpinnipati@gmail.com>
2020-10-14 13:24:03 -0700

    Preview height issue fixed.

--- src/CodeSnippetDisplay.tsx
+++ src/CodeSnippetDisplay.tsx
@@ -535,8 +535,18 @@ export class CodeSnippetDisplay extends React.Component<
   private _setPreviewPosition(id: string): void {
     const intID = parseInt(id, 10);
     const realTarget = document.getElementsByClassName(TITLE_CLASS)[intID];
+    const newTarget = document.getElementsByClassName('jp-codeSnippet-item')[
+      intID
+    ];
     // distDown is the number of pixels to shift the preview down
     const distDown: number = realTarget.getBoundingClientRect().top - 43; //this is bumping it up
+    const elementSnippet = newTarget as HTMLElement;
+    const heightSnippet = elementSnippet.clientHeight;
+    const heightPreview = heightSnippet.toString(10) + 'px';
+    document.documentElement.style.setProperty(
+      '--preview-max-height',
+      heightPreview
+    );
     // if (realTarget.getBoundingClientRect().top > window.screen.height / 2) {
     //   distDown = distDown - 66; //this is bumping it up further if it's close to the end of the screen
     // }
