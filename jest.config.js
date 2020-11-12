909c0acd3e22b2195a6e9159789232e7c3cfe1a0
jahn <aju960219@gmail.com>
2020-11-12 10:39:26 -0800

    Remove unnecessary setup

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  roots: ['<rootDir>'],
   verbose: true,
   preset: 'ts-jest/presets/js-with-babel',
-  // moduleNameMapper: {
-  //   '\\.(css|less|sass|scss)$': '<rootDir>/',
-  //   '\\.(gif|ttf|eot)$': '@jupyterlab/testutils/lib/jest-file-mock.js'
-  // },
   transform: {
-    // '\\.svg$': 'jest-raw-loader',
-    // '^.+\\.md?$': 'markdown-loader-jest',
     '^.+\\.tsx?$': 'ts-jest'
   },
   setupFiles: ['<rootDir>/testutils/jest-setup-files.js'],
