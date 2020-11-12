303cda560df32510d6e8feaa2666bcc41b4998d5
jahn <aju960219@gmail.com>
2020-11-12 10:59:04 -0800

    Unit test for CodeSnippetWidgetModel class

--- test/codeSnippetWidgetModel.test.ts
+++ test/codeSnippetWidgetModel.test.ts
@@ -56,15 +56,96 @@ describe('test getter and setter', () => {
   });
 });
 
-// describe('test snippet manipulation', () => {
-//   it('test add snippet', () => {
-//     const newSnippet = {
-//       name: 'test_snippet_three',
-//       description: 'testing code snippet widget model',
-//       language: 'Python',
-//       code: ["print('testing')"],
-//       id: 2
-//     };
-//     codeSnippetWidgetModel.addSnippet(newSnippet);
-//   });
-// });
+describe('test snippet manipulation', () => {
+  it('test add insert snippet', () => {
+    const insertSnippet = jest.spyOn(
+      CodeSnippetWidgetModel.prototype as any,
+      'insertSnippet'
+    );
+
+    const newSnippet = {
+      name: 'test_snippet_three',
+      description: 'testing code snippet widget model',
+      language: 'Python',
+      code: ["print('testing')"],
+      id: 2
+    };
+    codeSnippetWidgetModel.addSnippet(newSnippet, newSnippet.id);
+
+    expect(
+      codeSnippetWidgetModel.snippets[
+        codeSnippetWidgetModel.snippets.length - 1
+      ]
+    ).toBe(newSnippet);
+
+    expect(insertSnippet).toHaveBeenCalled();
+  });
+
+  it('test delete snippet', () => {
+    const originalSnippet = [
+      {
+        name: 'test_snippet_one',
+        description: 'testing code snippet widget model',
+        language: 'Python',
+        code: ["print('hello world')"],
+        id: 0,
+        tags: ['test']
+      },
+      {
+        name: 'test_snippet_two',
+        description: 'testing code snippet widget model',
+        language: 'Python',
+        code: ["print('hello world again!')"],
+        id: 1
+      }
+    ];
+    codeSnippetWidgetModel.deleteSnippet(-1);
+    expect(codeSnippetWidgetModel.snippets).toStrictEqual(originalSnippet);
+  });
+
+  it('test move snippet', () => {
+    codeSnippetWidgetModel.moveSnippet(0, 3);
+    expect(codeSnippetWidgetModel.snippets[1].id).toBe(2);
+  });
+
+  it('test reorder snippet', () => {
+    codeSnippetWidgetModel.reorderSnippet();
+    expect(codeSnippetWidgetModel.snippets[1].id).toBe(1);
+  });
+
+  it('test rename snippet', () => {
+    codeSnippetWidgetModel.renameSnippet('test_snippet_one', 'test_snippet');
+
+    expect(codeSnippetWidgetModel.snippets[1].name).toBe('test_snippet');
+  });
+
+  it('test sort snippet', () => {
+    const unsortedSnippets: ICodeSnippet[] = [
+      {
+        name: 'test_snippet',
+        description: 'testing code snippet widget model',
+        language: 'Python',
+        code: ["print('hello world')"],
+        id: 1,
+        tags: ['test']
+      },
+      {
+        name: 'test_snippet_two',
+        description: 'testing code snippet widget model',
+        language: 'Python',
+        code: ["print('hello world again!')"],
+        id: 0
+      }
+    ];
+    codeSnippetWidgetModel.snippets = unsortedSnippets;
+
+    codeSnippetWidgetModel.sortSnippets();
+    expect(codeSnippetWidgetModel.snippets[0].id).toBe(0);
+  });
+
+  it('test clear snippet', () => {
+    codeSnippetWidgetModel.clearSnippets();
+
+    expect(codeSnippetWidgetModel.snippets.length).toBe(0);
+  });
+});
